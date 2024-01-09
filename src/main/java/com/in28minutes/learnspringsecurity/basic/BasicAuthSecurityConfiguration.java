d57d03048b268b5a38d276cdcf2a8b612ebc5ad9
jeong <chung_junhuck@infonic.co.jp>
2024-01-09 16:16:42 +0900

    Store Credentails in H2Database: use JdbcDaoImpl default ddl to make schema. use JdbcUserDetailsManager to Store credentail info

--- src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -2,14 +2,20 @@ package com.in28minutes.learnspringsecurity.basic;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,11 +35,43 @@ public class BasicAuthSecurityConfiguration {
 //		http.csrf().disable();
 		http.csrf((csrf) -> csrf.disable());
 
+//		use frameOptions to show H2Console
+//		http.headers().frameOptions().sameOrigin();
+		http.headers((headers) -> headers.frameOptions((frame) -> frame.sameOrigin()));
 		return http.build();
 	}
-	
+
+// InMemory User
+//	@Bean
+//	public UserDetailsService userDetailService() {
+//		
+//		var user = User.withUsername("in28minutes")
+//						.password("{noop}dummy")
+//						.roles("USER")
+//						.build();
+//		
+//		var admin = User.withUsername("admin")
+//						.password("{noop}dummy")
+//						.roles("ADMIN")
+//						.build();
+//		
+//		
+//		
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+
+//  from JdbcDaoImpl, make schema in H2Database
 	@Bean
-	public UserDetailsService userDetailService() {
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+					.setType(EmbeddedDatabaseType.H2)
+					.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+					.build();
+	}
+	
+//  use JdbcUserDetailsManager, store same user Credentials into H2Database
+	@Bean
+	public UserDetailsService userDetailService(DataSource dataSource) {
 		
 		var user = User.withUsername("in28minutes")
 						.password("{noop}dummy")
@@ -45,8 +83,11 @@ public class BasicAuthSecurityConfiguration {
 						.roles("ADMIN")
 						.build();
 		
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
 		
-		
-		return new InMemoryUserDetailsManager(user, admin);
+		return jdbcUserDetailsManager;
 	}
+
 }
