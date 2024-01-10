20f37fda2026db0fea97aa9e4fc2603aac220e98
jeong <chung_junhuck@infonic.co.jp>
2024-01-10 22:50:20 +0900

    Spring Security: MethodLevel annotations

--- src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
@@ -19,13 +20,19 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-// @Configuration
+@Configuration
+@EnableMethodSecurity(jsr250Enabled=true, securedEnabled=true)
 public class BasicAuthSecurityConfiguration {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+		http.authorizeHttpRequests(
+				(requests) -> requests
+								// use RequestMatchers: can set specific rules
+								.requestMatchers("/users").hasRole("USER")
+								.requestMatchers("/admin/").hasRole("USER")
+								.anyRequest().authenticated());
 		http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		http.httpBasic(withDefaults());
 		
